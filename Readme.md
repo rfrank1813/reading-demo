46a066342123d74b3cd62fce6084a40adfab1afd
Chris Frank <chrisfrank@Backups-MacBook-Pro.local>
2020-02-18 12:03:40 -1000

    demo working with web sockets and half-decent word list

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Run npm i 
+# node . 
+# Server will start on port 8080 
+
+# Load the student side on a laptop by going to localhost:8080/student 
+# Because the browser won't allow remote triggered sounds, you need to click the button 
+
+
+# Load the teacher side on your mobile by going to your.local.ip.address:8080/teacher 
+# Use the +1 and -1 buttons to grade correct and incorrect. They should play sounds on the student UI when tapped 
+# Tap a word from the list to give it as an exercise to the student 
+# Words get harder as you scroll down 
+
+
+
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const hostname = '0.0.0.0';
+const port = 8080;
+const WebSocket = require('ws'); 
+app.use(express.urlencoded());
+
+
+// Server static files and run server
+sendFileOptions = {
+  root: __dirname
+}
+
+app.use(express.static('public'))
+
+app.get('/student', (req, res) => 
+  res.sendFile('student.html', sendFileOptions))
+
+app.get('/teacher', (req,res) =>
+  res.sendFile('teacher.html', sendFileOptions))
+
+
+
+
+
+app.listen(port, () => console.log(`Started on port ${port}!`))
+
+
+// Establish web socket connection 
+// and listen for messages
+
+const wss = new WebSocket.Server({port: 82});
+
+var clients = []; 
+var count = 0;
+
+wss.on('connection', function(ws) { 
+  var num = count++; 
+  client = {
+    ws:ws,
+    id:num
+  }; 
+  
+  clients[num] = client; 
+  //ws.send("{msg:'got connection'}");
+
+  ws.on('message',function(msg){ 
+    for(var i in clients){ 
+      clients[i].ws.send(msg); 
+    } 
+  }); 
+
+  ws.on('close',function(msg){ 
+    delete clients[num]; 
+  });
+
+});
