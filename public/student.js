1974786692c6d9120a5ec990730c14200a34dd1c
Chris Frank <chrisfrank@Backups-MacBook-Pro.local>
2020-02-22 15:34:01 -1000

    refactor js into separate files. made override so parent can choose word to deliver. fixed bug for the 'o-e' grapheme in wordes like 'tone'

---
+++ public/student.js
@@ -0,0 +1,273 @@
+// HANDLE WEBSOCKETS 
+
+var connection = new WebSocket('ws://'+location.hostname+':82'); 
+
+connection.onopen = function (e) { 
+  console.log("connected "+e); 
+}; 
+
+connection.onerror = function (error) { 
+  console.log('WebSocket Error ' + error); 
+}; 
+
+connection.onmessage = function (e) { 
+
+  window.e = e 
+  let msg= JSON.parse(e.data); 
+  console.log("MSG: ", msg);
+  window.msg = msg;
+
+  // Handle everything else 
+  switch(msg.action) {
+    case 'hint': 
+      position = msg.data;
+      doHint(position);
+      break; 
+    case 'correct':
+      playSound("/positive.mp3")
+      updateStreakTracker(1);
+      break;
+    case 'incorrect':
+      playSound("/wrong.mp3")
+      updateStreakTracker(-1)
+      break;
+    case 'word':
+
+      // updateWord function pulls from here
+      window.new_word = msg.data.gpcs
+
+      // on first word, show to user
+      // all other times, wait for updateWord to be called from elsewhere 
+      if( firstWord || msg.data.forceUpdate ) {
+        firstWord = false
+        updateWord() 
+      } else {
+        // do nothing
+      }
+  }
+} 
+
+
+function playSound(url) {
+  var sound = new Howl({
+    src: [url]
+  });
+  sound.play();
+}
+
+// Just puts the word in html. Another function displays it. 
+function updateWord() {
+  
+  msg = new_word; 
+
+  // Parse string to array 
+  gpcs = msg
+
+  // How to handle o-e and such
+  // Well it's basically a trailing e always 
+  // So what you do is if you see that, you just split it 
+  // Put the first letter in right away
+  // Save the second letter and the position for later 
+  // Once you're done looping through, just look for a trailing e and pop it on if needed 
+  // Actually, it's not that
+  // You just want to put in a single consonant
+
+  // check for compound grapheme, like "o-e" in the word "tone"
+  compound_grapheme = false 
+
+  // Put each gpc of the word into a span 
+  html = "";
+  gpcs.forEach( function(gpc, i) {
+    gpc = gpc.split("~")
+    gr = gpc[0]
+    ph = gpc[1]
+
+    // check for trailing e
+    if( gr.includes("-") ) {
+      compound_grapheme = true 
+      gr = gr.split("-")[0]
+      compound_grapheme_class = `position${i}`
+    }
+    html = html + `<div style='display:inline-block' class="position${i}" phoneme="${ph}">${gr}</div>`
+
+  })
+
+  if( compound_grapheme ) {
+    html = html + `<div style='display:inline-block' class="${compound_grapheme_class}" >e</div>`
+  }
+
+  $("#word").html(html);
+
+  animateCSS('#word', 'zoomIn');
+
+}
+
+
+// What happens?
+
+// When the FIRST new word comes in, just call the updateWord function 
+
+// For all subsequent words, just set a window variable 
+
+// Get rid of the "pause" variable 
+
+// When an answer is given, do all your animation. At end of animation, call updateWord 
+
+
+
+function updateStreakTracker(direction) {
+
+  // Clear the word 
+  $("#word").html("")
+
+  if(direction == 1) {
+    // Add a smiley 
+    $('.streak-tracker').append("<img class='tracker-smiley' src='/happy.png'/>")
+
+    animateCSS("img:last-child", "tada", function() {
+    
+      // If 5 smileys, clear the tracker 
+      smileys = $(".streak-tracker").children().length
+      if(smileys >= 5) {
+        updateProgressBar(1)
+      } else {
+        updateWord()
+      }
+    })
+  }
+
+  if(direction == -1) {
+
+    // Add a sad face 
+    $('.streak-tracker').append("<img src='/dead.png'/>")
+
+    // Wait a tick
+    setTimeout(function() {
+
+      // Clear the streak tracker
+      animateCSS(".streak-tracker", "shake", function() {
+        $(".streak-tracker").children().remove()
+
+        // Update progress bar 
+        updateProgressBar(-1)
+      });
+    }, 1000);
+  }
+}
+
+
+function updateProgressBar(direction) {
+
+  // play a sound
+  if(direction > 0) {
+    playSound("/fanfare.m4a")
+  } else {
+    // do nothing
+  }
+  
+
+  // Hide the word
+  $("#word").html()
+
+  // get screen width 
+  screen_width = $(".progress-tracker").width() 
+
+  // use screen width to determine pixel width of increment 
+  section_width = Math.floor( screen_width / 20 ); 
+
+  // multiply by direction, either 1 or -1 
+  increment = section_width * direction 
+
+  // get current progress bar width 
+  current_width = $(".progress-tracker-inner").width() 
+
+  // add result to progress bar width
+  new_width = increment + current_width
+
+  // set width to 0 if calculated width is less than 0
+  if (new_width < 0) {
+    new_width = 0;
+  }
+
+  // Empty streak tracker
+  setTimeout(function(){
+    $(".streak-tracker").children().remove()
+
+    // update progress bar width
+    animateCSS(".progress-tracker", "pulse", function() {
+      $(".progress-tracker-inner").width(new_width);
+      setTimeout(updateWord, 500)
+    })
+  }, 1000)
+
+  
+  
+} 
+
+function doHint(position) {
+  selector = `.position${position}`
+
+  phoneme = $(selector).first().attr("phoneme")
+
+  // Play the sound
+  var sound = new Howl({
+    src: ["/phonemes/"+phoneme+".mp3"]
+  });
+  sound.play();
+
+  // Pulse the letters
+  animateCSS(selector, 'bounce');
+}
+
+function ready() {
+  $(".go-button").hide()
+  $("#word").html("wait...");
+}
+
+// displayButtons function
+function setup() {
+
+  // fetch the right data for the buttons
+  // add a db entry for id 0 on start state
+  // $.ajax({
+  //   url: "/get_word",
+  //   type: "GET",
+  //   success: function(data) {
+
+  //     window.data = data;
+
+  //     if(data.length==0) {
+  //       alert("No data!"); 
+  //     } 
+
+  //     $("#word").html(data.word);
+  //   }
+  // });
+
+}
+
+
+function animateCSS(selector, animationName, callback) {
+  
+  nodes = $(selector)
+
+  $.each(nodes, function( index, node) {
+
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+  }); 
+  
+}
+
+$(document).ready(function() {
+  setup();
+  firstWord = true
+});
---
+++ public/teacher.js
@@ -0,0 +1,166 @@
+
+// All my websocket stuff 
+var connection = new WebSocket('ws://'+location.hostname+':82'); 
+
+connection.onopen = function (e) { 
+  console.log("connected "+e); 
+}; 
+
+connection.onerror = function (error) { 
+  console.log('WebSocket Error ' + error); 
+}; 
+
+// Does nothing for now. 
+connection.onmessage = function (e) { 
+  console.log(e);
+  // let obj=JSON.parse(e.data); 
+  // console.log(obj);
+};
+
+// Sends message to the student app 
+// message must be a string 
+function pushMessage(action, data={}) {
+  msg = {}
+  msg.action = action 
+  msg.data = data
+  str = JSON.stringify(msg)
+  connection.send(str); 
+};
+
+// Update word 
+function updateWord (word, forceUpdate=false) {
+  
+  // update the hint container 
+  gpcs = wordList[word];
+  phonemes = gpcs.map(x => x.split("~")[1]);
+  graphemes = gpcs.map(x => x.split("~")[0]); 
+  console.log("phonemes: ", phonemes);
+
+  html = "";
+  phonemes.forEach( function(ph,i) {
+    if(i != 0) {
+      html = html + "<span>&bull;</span>";
+    }
+    html = html + `<span onclick='sendHint(${i})'>${graphemes[i]}</span>`
+  })
+  $(".hint-container").html(html);
+
+  $("#hint-message").show()
+
+
+  // send the word via pushMessage
+  console.log(gpcs);
+  window.gpcs = gpcs;
+  pushMessage("word", {gpcs: gpcs, forceUpdate: forceUpdate});
+}
+
+// Send a hint
+function sendHint(position) { 
+  pushMessage("hint", position);
+}
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+// Grade answer 
+function gradeAnswer(correct) {
+  if(correct) {
+    pushMessage('correct');
+    pickWord(1)
+  } else {
+    pushMessage('incorrect');
+    pickWord(0)
+  }
+}
+
+
+// Pick new word and send to student 
+function pickWord(correct) {
+
+  // set percentile if not set 
+  if (typeof percentile === 'undefined') {
+    percentile = 5; 
+  }
+
+
+  // start a streak counter if one not set 
+  if (typeof streak === 'undefined') {
+    streak = 0; 
+  }
+
+  // if correct, increment the streak. otherwise, reset streak and jump down a percentile level
+  if (correct) {
+    streak++; 
+  } else {
+    streak = 0;
+    percentile -= 5;
+  }
+
+  // tell them when they've won  
+  if (percentile == 100 && streak >=6) {
+    alert("You beat the game!");  
+  }
+
+  // if streak over 5, increment percentile by 5 and reset streak 
+  if(streak >=6) {
+    percentile += 5;
+    streak = 0;
+  }
+
+  // make sure percentile is positive 
+  if (percentile < 5) {
+    percentile = 5; 
+  }
+
+  // make sure percentile is not over 100 
+  if (percentile > 100) {
+    percentile = 100; 
+  }
+
+  // Get max and min index in wordList based on percentile 
+  num_words = Object.keys(wordList).length
+  max_index = Math.floor( num_words * (percentile/100) ) - 1
+  min_index = Math.floor( num_words * (percentile - 5) / 100 ) 
+
+  // pick randomly from within that index 
+  next_word_index = randomIntFromInterval(min_index, max_index)
+
+  word = Object.keys(wordList)[next_word_index];
+  
+  console.log("Percentile: ", percentile); 
+  console.log("Streak: ", streak); 
+  console.log("Index:", next_word_index);
+  console.log("word: ", word);
+
+  // set that word as the current word 
+  updateWord( word )
+}
+
+
+function setup() {
+
+  words = Object.keys(wordList); 
+
+
+  words.forEach(function(word) {
+    el = $("<li>" + word + "</li>" );
+    $("#wordList").append( el );
+  });
+
+
+  $( "#wordList li" ).click(function() {
+    word = $(this).text(); 
+    console.log(word);
+    updateWord(word, true);
+  });
+
+  
+}
+
+
+$(document).ready(function() {
+  setup();
+});
